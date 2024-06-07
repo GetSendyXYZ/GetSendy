3f32cc76a1171d38a1a738ae333de342af7d2de1
GetSendy <rich@getsendy.xyz>
2024-06-07 22:02:12 +0100

    Fix & Siging

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,9 +4,6 @@ import ErrorComponent from '@/components/Error';
 
 export default function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <ErrorComponent
-      title="Oops... We ran into a problem"
-      message={error.message}
-    />
+    <ErrorComponent title="We ran into a problem" message={error.message} />
   );
 }
