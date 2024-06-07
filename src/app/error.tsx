c71b302a616f6284e5e4c4b91d4fdb8539c53ad4
GetSendy <rich@getsendy.xyz>
2024-06-07 22:05:34 +0100

    Update Error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,6 +4,9 @@ import ErrorComponent from '@/components/Error';
 
 export default function ErrorBoundary({ error }: { error: Error }) {
   return (
-    <ErrorComponent title="We ran into a problem" message={error.message} />
+    <ErrorComponent
+      title="Oops... We ran into a problem"
+      message={error.message}
+    />
   );
 }
