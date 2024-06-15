99a715da28a08300655072ed1e8937cd921fd952
GetSendyXYZ <rich@getsendy.xyz>
2024-06-15 11:40:32 +0100

    Fix Incinerate On Mainnet (#16)

--- src/app/incinerate/layout.tsx
+++ src/app/incinerate/layout.tsx
@@ -1,9 +1,11 @@
 'use client';
+
 import { useAccountProvider } from '@/Providers/AccountProvider';
 import { Loader } from '@/components/Loader';
 import Loading from '@/components/Loading';
 import NotAuthed from '@/components/NotAuthed';
 import NotOnWhiteList from '@/components/NotOnWhiteList';
+import { env } from '@/env';
 import { useFutureverse } from '@futureverse/react';
 
 import { useAccount } from 'wagmi';
@@ -16,6 +18,27 @@ export default function RootLayout({
   const { userSession } = useFutureverse();
   const { isConnecting, isConnected } = useAccount();
   const { isWhitelisted, checkingWhitelist } = useAccountProvider();
+
+  const network = env.NEXT_PUBLIC_NETWORK;
+
+  if (network === 'root') {
+    return (
+      <Loader classes="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <Loader.Logo />
+        <Loader.Text text="Sorry, this is not available On mainnet"></Loader.Text>
+        <div>
+          <a
+            href="https://porcini.getsendy.xyz/incinerate"
+            target="_blank"
+            rel="noreferrer nofollow"
+            className="text-base underline cursor-pointer"
+          >
+            Try incinerate on porcini
+          </a>
+        </div>
+      </Loader>
+    );
+  }
 
   if (isConnecting) {
     return <Loading />;
